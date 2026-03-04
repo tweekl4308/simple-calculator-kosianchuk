edbadf27827cfbd354739109ac5dbe3dbb02b22a
Kosianchuk <kosianchuk@gmail.com>
2026-03-04 20:22:59 +0100

    init project structure

---
+++ main.py
@@ -0,0 +1,5 @@
+from calculator import add
+
+print("Application started")
+print("Result:", add(2,3))
+print("Calculation completed")
---
+++ validator.py
@@ -0,0 +1,2 @@
+def validate_number(value):
+    return isinstance(value, int) or isinstance(value, float)
