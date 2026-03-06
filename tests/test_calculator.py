99c1616259af4d91a7d65ecaadf3c7b219da45aa
Kosianchuk <kosianchuk@gmail.com>
2026-03-06 17:10:42 +0100

    fix flake8 errors

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,9 +1,11 @@
 import sys
 import os
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 from calculator import add
+
+sys.path.insert(
+    0,
+    os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+)
 
 
 def test_add():
